502aa7cf62a0e96e4c0dda168af04be38f94b9c3
Eduardo Mendes <edumendes@gmail.com>
2023-07-29 16:20:34 -0400

    keywords that can evidence severity level chang

---
+++ src/keywords.js
@@ -0,0 +1,29 @@
+const keywords = [
+  'debug',
+  'info',
+  'trace',
+  'fatal',
+  'error',
+  'warn',
+  'warning',
+  'logging',
+  '^log$',
+  'log level',
+  'severity level',
+  'change level',
+  'change severity',
+  'slf4j',
+  'log4j',
+  'commons-logging',
+  'logback',
+  'noisy',
+  'verbose',
+  'spammy',
+  'overload',
+  'fail',
+ 
+  // Add more keywords as needed
+]
+;
+
+export default keywords;
