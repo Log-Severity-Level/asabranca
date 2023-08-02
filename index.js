6d04e075d00fc4c72fbbda8a7b1549859015ddb9
Eduardo Mendes <edumendes@gmail.com>
2023-08-02 11:30:01 -0400

    Change the main operation to the script all

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { processFileUrls } from "./src/extract.js";
+import { processAll } from "./src/extract.js";
 
 const inputFilePath = process.argv[2];
 const outputFilePath = process.argv[3];
 
-processFileUrls(inputFilePath, outputFilePath);
+processAll(inputFilePath, outputFilePath);
