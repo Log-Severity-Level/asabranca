0c08141ecbe1ace6e0bbb9e244846f217348c6af
Eduardo Mendes <edumendes@gmail.com>
2023-08-03 15:18:04 -0400

    Import correction

--- extract-jira-details-from-id.js
+++ extract-jira-details-from-id.js
@@ -1,5 +1,6 @@
-import { extractJiraIssuesDetailsFromIdsToCsv } from './src/extractor';
+import { extractJiraIssuesDetailsFromIdsToCsv } from "./src/extractor.js";
 
 const inputFilePath = process.argv[2];
 const outputFilePath = process.argv[3];
+
 extractJiraIssuesDetailsFromIdsToCsv(inputFilePath, outputFilePath)
